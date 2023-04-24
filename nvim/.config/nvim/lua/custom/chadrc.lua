70976fa9d104a09d91dd7cf38fa4cbf18ba11d4f
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-04-24 09:43:46 +0500

    removed theme_toggle in nvim

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -6,7 +6,6 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "tokyonight",
-  -- theme_toggle = { "kanagawa", "tokyodark" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
