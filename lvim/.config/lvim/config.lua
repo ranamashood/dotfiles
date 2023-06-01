53145743e79a89b91e1e9332977c5461a7038ebe
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-06-01 18:11:47 +0500

    added new lvim config

---
+++ lvim/.config/lvim/config.lua
@@ -0,0 +1,21 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.relativenumber = true
+
+lvim.colorscheme = "tokyonight-night"
+lvim.format_on_save = true
+
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<M-S-h>"] = ":BufferLineMovePrev<CR>"
+lvim.keys.normal_mode["<M-S-l>"] = ":BufferLineMoveNext<CR>"
+
+lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
+lvim.builtin.terminal.open_mapping = "<M-v>"
+
+lvim.plugins = {
+  -- { "wakatime/vim-wakatime" }
+}
