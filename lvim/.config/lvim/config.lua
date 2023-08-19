de085c87d65ac3293b1c8989ea02e9b41eb6057b
Mashood ur Rehman Rana <ranamashood@proton.me>
2023-08-19 12:57:43 +0500

    added many plugins in lvim and dap configuration
    dap configuration for c, c++, rust(not tested)
    added formatters chsharpier and prettierd(for fast formatting as this is a daemon for prettier)

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -36,5 +36,137 @@ lvim.plugins = {
     config = function()
       require 'nvim-lastplace'.setup {}
     end
+  },
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require("flutter-tools").setup {
+        widget_guides = {
+          enabled = true,
+        },
+      }
+    end,
+  },
+  {
+    "nvim-orgmode/orgmode",
+    lazy = false,
+    config = function()
+      -- Load custom treesitter grammar for org filetype
+      require('orgmode').setup_ts_grammar()
+
+      -- Treesitter configuration
+      require('nvim-treesitter.configs').setup {
+        -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+        -- highlighting will fallback to default Vim syntax highlighting
+        highlight = {
+          enable = true,
+          -- Required for spellcheck, some LaTex highlights and
+          -- code block highlights that do not have ts grammar
+          additional_vim_regex_highlighting = { 'org' },
+        },
+        ensure_installed = { 'org' }, -- Or run :TSUpdate org
+      }
+
+      require('orgmode').setup({
+        org_agenda_files = { '~/org' },
+      })
+    end
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = false,
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        autotag = {
+          enable = true,
+        }
+      }
+      require('nvim-ts-autotag').setup()
+    end
   }
 }
+
+local dap = require('dap')
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = os.getenv("HOME") .. '/.local/share/lvim/mason/bin/codelldb',
+    args = { "--port", "${port}" },
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      -- return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      return vim.fn.getcwd() .. '/builtFile'
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    name = "prettierd",
+    filetypes = { "typescript", "typescriptreact", "css" },
+  },
+  {
+    name = "csharpier",
+    filetypes = { "cs" },
+  },
+}
+
+
+local lspconfig = require('lspconfig')
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "astro,css,eruby,html,htmldjango,javascriptreact,less,pug,sass,scss,svelte,typescriptreact,vue",
+  callback = function()
+    vim.lsp.start({
+      cmd = { "emmet-language-server", "--stdio" },
+      root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
+      init_options = {
+        --- @type table<string, any> https://docs.emmet.io/customization/preferences/
+        preferences = {},
+        --- @type "always" | "never" defaults to `"always"`
+        showexpandedabbreviation = "always",
+        --- @type boolean defaults to `true`
+        showabbreviationsuggestions = true,
+        --- @type boolean defaults to `false`
+        showsuggestionsassnippets = false,
+        --- @type table<string, any> https://docs.emmet.io/customization/syntax-profiles/
+        syntaxprofiles = {},
+        --- @type table<string, string> https://docs.emmet.io/customization/snippets/#variables
+        variables = {},
+        --- @type string[]
+        excludelanguages = {},
+      },
+    })
+  end,
+})
+
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "dartls" })
+
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "csharp-ls" })
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "csharp-ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
