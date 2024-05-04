4d012e0f40cdac9e0e2ce24bcafd8f94a5ae1a04
Mashood ur Rehman Rana <ranamashood@proton.me>
2024-05-05 00:24:20 +0500

    added new keybindings for nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,12 @@
 vim.keymap.set({ "n" }, "<leader>ml", "<cmd>LiveServer<cr>", { silent = true })
+vim.keymap.set({ "n" }, "<leader>mf", "<cmd>Telescope flutter commands<cr>", { silent = true })
+
+vim.keymap.set("n", "<A-S-j>", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("n", "<A-S-k>", ":m .-2<CR>==", { silent = true })
+vim.keymap.set("v", "<A-S-j>", ":m '>+1<CR>gv-gv", { silent = true })
+vim.keymap.set("v", "<A-S-k>", ":m '<-2<CR>gv-gv", { silent = true })
+
+vim.keymap.set("n", "<A-S-h>", ":BufferLineMovePrev<cr>", { silent = true })
+vim.keymap.set("n", "<A-S-l>", ":BufferLineMoveNext<cr>", { silent = true })
+
+vim.keymap.set("n", "<S-u>", ":Telescope undo<cr>", { silent = true })
