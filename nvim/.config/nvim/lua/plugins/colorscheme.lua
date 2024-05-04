600f5982c7fb607459231e930bc54c5e34149d69
Mashood ur Rehman Rana <ranamashood@proton.me>
2024-05-05 00:23:54 +0500

    moved nvim colorscheme config to new file

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,11 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "moonfly",
       colorscheme = "tokyonight-night",
       -- colorscheme = "catppuccin",
     },
   },
+
+  -- { "bluz71/vim-moonfly-colors", name = "moonfly", priority = 1000 },
 }
