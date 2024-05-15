256ada6d2dfddcda319774273e41380c03a1fbc6
Mashood ur Rehman Rana <ranamashood@proton.me>
2024-05-15 20:34:43 +0500

    show workspaces (status bar) in sorted asc order

--- ags/.config/ags/config.js
+++ ags/.config/ags/config.js
@@ -15,13 +15,15 @@ const date = Variable("", {
 function Workspaces() {
   const activeId = hyprland.active.workspace.bind("id");
   const workspaces = hyprland.bind("workspaces").as((ws) =>
-    ws.map(({ id }) =>
-      Widget.Button({
-        on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-        child: Widget.Label(`${id}`),
-        class_name: activeId.as((i) => `${i === id ? "focused" : ""}`),
-      }),
-    ),
+    ws
+      .sort((a, b) => a.id - b.id)
+      .map(({ id }) =>
+        Widget.Button({
+          on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+          child: Widget.Label(`${id}`),
+          class_name: activeId.as((i) => `${i === id ? "focused" : ""}`),
+        }),
+      ),
   );
 
   return Widget.Box({
