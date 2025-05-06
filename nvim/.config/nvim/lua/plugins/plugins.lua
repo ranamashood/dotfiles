c6e1b34571888d9cf316c3ca731898d8d1888d85
Mashood ur Rehman Rana <ranamashood@proton.me>
2025-05-06 16:27:23 +0500

    last commit before migrating config

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -112,13 +112,13 @@ return {
       require("colorizer").setup()
     end,
   },
-  {
-    "akinsho/flutter-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("flutter-tools").setup({})
-    end,
-  },
+  -- {
+  --   "akinsho/flutter-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   config = function()
+  --     require("flutter-tools").setup({})
+  --   end,
+  -- },
   {
     "telescope.nvim",
     dependencies = {
